2b0b122218d9be3032f9cbe2ff57904221ba9797
JG <jgalan@iesguadalquivir.net>
2023-02-05 00:26:03 +0000

    a

--- pages/Alumnos/jsobjects/JSObject1/JSObject1.js
+++ pages/Alumnos/jsobjects/JSObject1/JSObject1.js
@@ -3,7 +3,7 @@ export default {
 	myVar2: {},
 	autorizado: () => {
 		//write code here
-		if(CompruebaAutorizados.data.some(e => e[0] === email_logueado.text))
+		if(CompruebaAutorizados.data.values.some(e => e[0] === email_logueado.text))
 			return "logedin";
 		else
 			return "notlogedin";
